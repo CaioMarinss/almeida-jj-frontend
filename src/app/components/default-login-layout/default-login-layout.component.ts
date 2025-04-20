db4addce9376c95e38276e1e70f11ebdc3fd9e33
Caio Marins <marinsroyale@gmail.com>
2025-04-20 01:56:21 -0300

    feat: finalizando layout

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
   templateUrl: './default-login-layout.component.html',
-  imports: []
+  imports: [],
+  styleUrls: ['./default-login-layout.component.scss']  // Adicionei o caminho do CSS aqui
 })
 export class DefaultLoginLayoutComponent {
+  //variaveis q vou receber de quem me usar de layout
+  @Input() title: string = "";
+  @Input() primeiroBotao: string = "";
+  @Input() segundoBotao: string = "";
+
   }
 
