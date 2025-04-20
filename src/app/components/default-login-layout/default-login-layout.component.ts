348fa9a23768121fab7f9383fadefe4da8c87d2e
Caio Marins <marinsroyale@gmail.com>
2025-04-20 00:45:47 -0300

    feat: criando a estrutura

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  standalone: true,
+  templateUrl: './default-login-layout.component.html',
+  imports: []
+})
+export class DefaultLoginLayoutComponent {
+  }
+
